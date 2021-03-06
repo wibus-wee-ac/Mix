e560c4460c7d4bdab927ec3379c56f4a71cb65c8
wibus-wee <1596355173@qq.com>
2021-03-06 10:48:00 +0800

    修复重要错误

--- Mix/functions.php
+++ Mix/functions.php
@@ -11,7 +11,7 @@ ini_set('display_errors', 0);
 
 require("libs/libs.php"); //最主要依赖
 require("libs/Utils.php"); //Utils，在libs.php中需要使用
-require("libs/functions_helper.php"); //other function
+require("libs/functions_helper.php"); //一些额外的functions
 require("libs/plugins_helper.php"); //使用插件接口实现的东西
 require("Core/Backup.php"); //备份
 require("functions_Config.php"); //模板外观设置
