917cdefcaa3597a4532f80edc9cb98a3988f25c1
Wibus <1596355173@qq.com>
2021-03-28 13:45:54 +0800

    Add a good idea

--- Mix/Core/globals.php
+++ Mix/Core/globals.php
@@ -4,11 +4,12 @@
  * @author: Wibus
  * @Date: 2021-03-15 22:51:31
  * @LastEditors: Wibus
- * @LastEditTime: 2021-03-21 10:55:29
+ * @LastEditTime: 2021-03-27 15:04:32
  */
-if (Typecho_Widget::widget('Widget_Options')->UseCDNAssets) {
-    $GLOBALS['assetURL'] = Typecho_Widget::widget('Widget_Options')->CDNURL;
+$GLOBALS['options'] = Typecho_Widget::widget('Widget_Options');
+if ($GLOBALS['options']->UseCDNAssets) {
+    $GLOBALS['assetURL'] = $GLOBALS['options']->CDNURL;
 }else{
-    $GLOBALS['assetURL'] = Typecho_Widget::widget('Widget_Options')->themeUrl.'/assets/';
+    $GLOBALS['assetURL'] = $GLOBALS['options']->themeUrl.'/assets/';
 }
 ?>
