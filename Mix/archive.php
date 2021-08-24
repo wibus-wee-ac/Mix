67172971fd580f8388801d31bcf7155ea442239c
wibus-wee <1596355173@qq.com>
2021-08-24 17:13:27 +0800

    优化纯文字的显示

--- Mix/archive.php
+++ Mix/archive.php
@@ -5,7 +5,7 @@ $num = 0;
 <meta name="description" content="<?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
-            'tag'       =>  _t('%s 下的文章'),
+            'tag'       =>  _t('标签 %s 下的文章'),
             'author'    =>  _t('%s 发布的文章')
         ), '', ' - '); ?>">
 <!--头部必要元素-->
--- Mix/component/index/text/show_article.php
+++ Mix/component/index/text/show_article.php
@@ -15,13 +15,13 @@
 <div class="show-article-main">
 <?php if ($this->have()): ?>
 <?php while($this->next()): ?>
-        <div class="wrap min show-article" style="animation: <?php $this->options->IndexAction()?>">
+        <div class="wrap show-article" style="animation: <?php $this->options->IndexAction()?>">
             <h2><?php $this->title() ?></h2>
             <p style="color: rgba(0, 2, 0, 0.199);"><?php $this->excerpt(300, '...'); ?></p>
             <a class="btn yellow" href="<?php $this->permalink() ?>">阅读全文</a>
 </div>
+<?php endwhile; ?>
 <?php $this->pageLink('下一页','next'); ?>
 <?php $this->pageLink('上一页'); ?>
-<?php endwhile; ?>
-<?php else: ?>暂无文章<?php endif; ?>
+<?php endif; ?>
 </div>
